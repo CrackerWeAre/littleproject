b0ead46aaf35f7e17c9a7fc82d9ace2a68a5b097
정윤도 <truenorthj@gmail.com>
2020-05-17 22:03:16 +0900

    플랫폼입력란 수정

--- frontend/client/src/components/admins/AdminForm.js
+++ frontend/client/src/components/admins/AdminForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { white } from 'color-name';
 
 class StreamForm extends React.Component{
 
@@ -7,7 +8,7 @@ class StreamForm extends React.Component{
         if(touched && error){
             return (
                 <div className="ui error message">
-                    <div className="header">{error}
+                    <div className="ui error meesage" style={{background : "white"}}>{error}
                     </div>
                 </div>
             )
@@ -25,6 +26,24 @@ class StreamForm extends React.Component{
         )
     }
 
+    renderSelect = ({input, label, meta}) => {
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`
+        return (
+            <div className={className}>
+                <label>{label}</label>
+                <select {...input} autoComplete="off">
+                    <option></option>
+                    <option value="youtube">Youtube</option>
+                    <option value="twitch">Twitch</option>
+                    <option value="afreecatv">Afreecatv</option>
+                    <option value="vlive">Vlive</option>
+                </select>
+                {this.renderError(meta)}
+            </div>
+            
+        )
+    }
+
     onSubmit = (formValues) => {
         this.props.onSubmit(formValues)
     }
@@ -32,7 +51,10 @@ class StreamForm extends React.Component{
     render(){
         return (
             <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
-                <Field name="platform" component={this.renderInput} label="Enter platform"/>
+            
+                <Field name="platform" component={this.renderSelect} label="Enter platform">
+                
+                </Field>
                 <Field name="channel" component={this.renderInput} label="Enter Channel"/>
                 <Field name="channelID" component={this.renderInput} label="Enter channelID"/>
                 <button className="ui button primary">Submit</button>
@@ -42,17 +64,17 @@ class StreamForm extends React.Component{
 }
 
 const validate = (formValues) => {
+    
     const errors = {};
     if (!formValues.platform){
-        errors.title = "you must enter a paltform"
+        errors.platform = "you must enter a platform"
     } 
     if (!formValues.channel){
-        errors.description = "you must enter a description"
+        errors.channel = "you must enter a channel"
     }
     if (!formValues.channelID){
-        errors.description = "you must enter a description"
+        errors.channelID = "you must enter a channelID"
     }
-
     return errors
 }
 
