b199778c65e365eb7dbfb780ec744b132bac872b
Primadonna <jwhyun2215@gmail.com>
2020-06-01 23:39:34 +0900

    login form 변경

--- frontend/client/src/components/login/Login.js
+++ frontend/client/src/components/login/Login.js
@@ -17,20 +17,36 @@ class Login extends Component {
         }
 
         return (
-            <div className="login">
-                
-                <GoogleLogin 
-                    clientId="845969621905-p9iupf8qgkmucm6d4978cls28lk6b84n.apps.googleusercontent.com"
-                    render={renderProps => (
-                        <button onClick={renderProps.onClick} disabled={renderProps.disabled}>구글로 로그인하기</button>
-                      )}
-                    buttonText="구글아이디로 로그인 하기"
-                    onSuccess={responseGoogle}
-                    onFailure={failure}
-                    cookiePolicy={'single_host_origin'}
-                >
-                </GoogleLogin>
+            <div className="signin-form">
+                <form>
+                <h2>Sign In</h2>
+                <p className="hint-text">Sign in with your social media account</p>
+                <div className="social-btn">
+                    <div className="login">
+                        <GoogleLogin 
+                            clientId="845969621905-p9iupf8qgkmucm6d4978cls28lk6b84n.apps.googleusercontent.com"
+                            render={renderProps => (
+                                <button id="google" className="login100-social-item" title="Google" onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                                    <img src="icon-google.png" alt="GOOGLE"></img>
+                                </button>
+                            )}
+                            buttonText="구글아이디로 로그인 하기"
+                            onSuccess={responseGoogle}
+                            onFailure={failure}
+                            cookiePolicy={'single_host_origin'}
+                        >
+                        </GoogleLogin>
+                    </div>
+                    <div className="login">
+                        <a href="#" id="facebook" className="login100-social-item">
+                            <img src="icon-facebook.png" alt="FACEBOOK"></img>
+                        </a>
+                    </div>
+                </div>
+                </form>
             </div>
+
+
         )
     }
 }
