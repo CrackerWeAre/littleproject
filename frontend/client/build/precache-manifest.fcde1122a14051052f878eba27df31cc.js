f4612bf72da6c2e6d66d45f38fe6884d7022a07b
정윤도 <truenorthj@gmail.com>
2020-06-03 22:56:58 +0900

    build추가

---
+++ frontend/client/build/precache-manifest.fcde1122a14051052f878eba27df31cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed1a820d9b6b8a38a7b8e8e523329ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7905628cd3d9d529caf2",
+    "url": "/static/css/main.fa79ec54.chunk.css"
+  },
+  {
+    "revision": "7809d6533c2c52a65e3c",
+    "url": "/static/js/2.eec5df16.chunk.js"
+  },
+  {
+    "revision": "0c3380804e3d32b3b313e82e80bde0ff",
+    "url": "/static/js/2.eec5df16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7905628cd3d9d529caf2",
+    "url": "/static/js/main.57c3cd4e.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  },
+  {
+    "revision": "431eccdf4226ea1dbbdd069e1787c06f",
+    "url": "/static/media/logo.431eccdf.png"
+  }
+]);
