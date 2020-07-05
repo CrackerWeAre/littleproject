9981a02beb58edd97894aef2d432139d2acdafce
정윤도 <truenorthj@gmail.com>
2020-07-05 15:50:27 +0900

    build

---
+++ frontend/client/build/precache-manifest.d0ec3c331f981d3df830d152fc967351.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8898d978994828ce48e2c230ea5e3293",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3e4f94f8fe394ff5b97",
+    "url": "/static/css/main.be01e725.chunk.css"
+  },
+  {
+    "revision": "029df0efb271820ce651",
+    "url": "/static/js/2.eb99cc1a.chunk.js"
+  },
+  {
+    "revision": "0c3380804e3d32b3b313e82e80bde0ff",
+    "url": "/static/js/2.eb99cc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e4f94f8fe394ff5b97",
+    "url": "/static/js/main.7fabb93b.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  },
+  {
+    "revision": "25ab3626b77d039abe87a29ef5e78de9",
+    "url": "/static/media/NotoSansKR-Black.25ab3626.otf"
+  },
+  {
+    "revision": "c4723de3da750cff5d95b7c9b398d983",
+    "url": "/static/media/NotoSansKR-Bold.c4723de3.otf"
+  },
+  {
+    "revision": "1b0504a3e741f8e6e07b297cb8f55d2d",
+    "url": "/static/media/NotoSansKR-Light.1b0504a3.otf"
+  },
+  {
+    "revision": "3e344d255a9c2bc603683f5b078d471d",
+    "url": "/static/media/NotoSansKR-Medium.3e344d25.otf"
+  },
+  {
+    "revision": "839a2f6c607e26dee10678a077729ba5",
+    "url": "/static/media/NotoSansKR-Regular.839a2f6c.otf"
+  },
+  {
+    "revision": "3c91862c95752f24cd41785301373e7b",
+    "url": "/static/media/NotoSansKR-Thin.3c91862c.otf"
+  },
+  {
+    "revision": "61e6ae1e1a1f4a7b6418bf2fe08812be",
+    "url": "/static/media/logo.black.61e6ae1e.png"
+  }
+]);
