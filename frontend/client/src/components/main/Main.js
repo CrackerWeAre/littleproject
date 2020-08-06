9b101d8b012de36b08f6af15b81cc6999e12f7b3
정윤도 <truenorthj@gmail.com>
2020-08-06 10:34:13 +0900

    resign 중복 제거

--- frontend/client/src/components/main/Main.js
+++ frontend/client/src/components/main/Main.js
@@ -8,13 +8,16 @@ import AirFrame from '../air/AirFrame'
 
 
 const Main = (props) => {
-    
+    const nowUniTime = new Date().getTime()/1000
     useEffect(() => {
         props.fetchAirs();
         if(props.user.userInfo!==undefined){
             props.fetchFollowingAirs(props.user.userInfo.email);
             props.fetchBlockedAirs(props.user.userInfo.email);
-            props.resignIn(props.user.userInfo)
+            if(props.user.userInfo.token_exp-nowUniTime<86400){
+                console.log("resignIn")
+                props.resignIn(props.user.userInfo)
+            }
         }
     }, [])
 
