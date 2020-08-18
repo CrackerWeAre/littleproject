6c8c9028d5a0a048a5bf07fbde8e49bb25bbe4c1
정윤도 <truenorthj@gmail.com>
2020-08-18 16:20:29 +0900

    div 수정

--- frontend/client/src/components/login/SignUp.js
+++ frontend/client/src/components/login/SignUp.js
@@ -46,36 +46,35 @@ export const SignUp = () => {
     }
 
     return (
-        <>
         
-        <form onSubmit={onSubmit} style={{padding:10}}>
-            <div>
-                <label htmlFor="user-id">아이디</label><br/>
-                <input name="user-id" value={id} required onChange={onChangeId} />
-            </div>
-            <div>
-                <label htmlFor="user-nick">닉네임</label><br/>
-                <input name="user-nick" value={nick} required onChange={onChangeNick} />
-            </div>
-            <div>
-                <label htmlFor="user-password">비밀번호</label><br/>
-                <input name="user-password" type="password" value={password} required onChange={onChangePassword} />
-            </div>
-            <div>
-                <label htmlFor="user-password-check">비밀번호체크</label><br/>
-                <input name="user-password-check" type="password" value={passwordCheck} required onChange={onChangePasswordChk} />
-                {passwordError && <div style={{color : 'red'}}>비밀번호가 일치하지 않습니다.</div>}
-            </div>
-            <div>
-                <checkbox name="user-term" value={term} onChange={onChangeTerm}>동의 합니까?</checkbox>
-                {termError && <div style={{color : 'red'}}>약관에 동의하셔야 합니다.</div>}
-            </div>
-            <div style={{marginTop:10}}>
-                <button type="primary" htmlType="submit" >가입하기</button>
-            </div>
-        </form>
-        
-        </>
+        <div className="signup_container">
+            <form onSubmit={onSubmit} style={{padding:10}}>
+                <div className="signup_content">
+                    <label htmlFor="user-id">아이디</label><br/>
+                    <input name="user-id" value={id} required onChange={onChangeId} />
+                </div>
+                <div className="signup_content">
+                    <label htmlFor="user-nick">닉네임</label><br/>
+                    <input name="user-nick" value={nick} required onChange={onChangeNick} />
+                </div>
+                <div className="signup_content">
+                    <label htmlFor="user-password">비밀번호</label><br/>
+                    <input name="user-password" type="password" value={password} required onChange={onChangePassword} />
+                </div>
+                <div className="signup_content">
+                    <label htmlFor="user-password-check">비밀번호체크</label><br/>
+                    <input name="user-password-check" type="password" value={passwordCheck} required onChange={onChangePasswordChk} />
+                    {passwordError && <div style={{color : 'red'}}>비밀번호가 일치하지 않습니다.</div>}
+                </div>
+                <div className="signup_content">
+                    <checkbox name="user-term" value={term} onChange={onChangeTerm}>동의 합니까?</checkbox>
+                    {termError && <div style={{color : 'red'}}>약관에 동의하셔야 합니다.</div>}
+                </div>
+                <div  className="signup_content" style={{marginTop:10}}>
+                    <button type="primary" htmlType="submit" >가입하기</button>
+                </div>
+            </form>
+        </div>
     );
 }
 
