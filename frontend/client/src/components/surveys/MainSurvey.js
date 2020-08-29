44e826e228741af0d236278c861628c9a3c563f2
HayeonBaek <bhy0512@gmail.com>
2020-08-30 00:46:07 +0900

    Survey 전체 프레임 수정 및 글로벌 스타일 추가

--- frontend/client/src/components/surveys/MainSurvey.js
+++ frontend/client/src/components/surveys/MainSurvey.js
@@ -1,126 +1,30 @@
-import React, { } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
-// import '../../style/css/Survey.css'
-import { Link } from "react-router-dom";
-import styled from 'styled-components';
+import '../../style/css/MainSurvey.css';
 
-import Slider from './Slider';
+import SurveyHeader from './SurveyHeader';
+import Section1 from './Section1';
+import Section2 from './Section2';
+import Section3 from './Section3';
+import Section4 from './Section4';
+import Section5 from './Section5';
+import Section6 from './Section6';
+import DotNavigation from './DotNavigation';
 
-const MainSurveyContainer = styled.div`
-    position: relative;
-    width: 100%;
-    margin-top: 50px;
-
-    h2 {
-        display: flex;
-        justify-content: center;
-        font-size: 2.5rem;
-        font-weight: 700;
-        line-height: 1.75;
-    }
-
-    p {
-        display: flex;
-        justify-content: center;
-        font-size: 1.5rem;
-        font-weight: 500;
-        line-height: 1.75;
-    }
-
-    div {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        margin-top: 20px;
-    }
-
-    button {
-        background: #fbb30f;
-        border-radius: 3px;
-        width: 190px;
-        height: 47px;
-        text-align: center;
-         
-        &:active {
-            outline: none;
-        }
-    }
-
-    button + button {
-        margin-left: 20px;
-    }
-`;
-
-const MainSurveyHeader = styled.header`
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 70px;
-    z-index: 9999;
-
-    h1 {
-        position: absolute;
-        top: 0;
-        left: 10%;
-        transform: translate(-50%, 70%);
-        margin-left: 30px;
-        font-size: 24px;
-        font-weight: 600;
-        line-height: 28px;
-    }
-
-    nav ul {
-        position: absolute;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        top: 0;
-        right: 10%;
-        transform: translate(50%, 50%);
-    }
-
-    nav ul li {
-        background: #fbb30f;
-        padding: 10px;
-        border-radius: 3px;
-    }
-
-    nav ul li a {
-        color: #000;
-    }
-
-`;
 
 export const MainSurvey = () => {
     return (
         <>
-            <MainSurveyHeader>
-                <h1>MeerkatOnAir</h1>
+            <SurveyHeader />
 
-                {/* <nav>
-                    <ul>
-                        <li>
-                            <a href="/">Get Started</a>
-                        </li>
-                    </ul>
-                </nav> */}
-            </MainSurveyHeader>
+            <Section1 />
+            <Section2 />
+            <Section3 />
+            <Section4 />
+            <Section5 />
+            <Section6 />
 
-            <Slider />
-
-            <MainSurveyContainer>
-                <h2>MeerkatOnAir에서 세상의 모든 온라인 라이브 방송을 찾아보세요.</h2>
-                <p>인기 라이브 방송부터 나에게 맞는 라이브 방송까지 모든 플랫폼의 라이브 방송을 한 곳에서 모아서 보세요.</p>
-                
-                <div>
-                    <button>설문조사 참여</button>
-                    <button>미어캣온에어 바로가기</button>
-                </div>
-
-            </MainSurveyContainer>
+            <DotNavigation />
         </> 
     );
 }
