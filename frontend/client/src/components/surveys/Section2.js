6e2657917c959ab2500214eaa442b111975b161b
HayeonBaek <bhy0512@gmail.com>
2020-10-08 21:32:22 +0900

    Section2 input checkbox 버그 수정

--- frontend/client/src/components/surveys/Section2.js
+++ frontend/client/src/components/surveys/Section2.js
@@ -6,163 +6,106 @@ import twitch from "../../style/img/platform/twitch.png";
 import youtube from "../../style/img/platform/youtube.png";
 import vlive from "../../style/img/platform/vlive.png";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGamepad } from "@fortawesome/free-solid-svg-icons";
-import { faHeadphones } from "@fortawesome/free-solid-svg-icons";
-import { faUtensils } from "@fortawesome/free-solid-svg-icons";
-import { faNewspaper } from "@fortawesome/free-solid-svg-icons";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import { faTv } from "@fortawesome/free-solid-svg-icons";
-import { faComments } from "@fortawesome/free-solid-svg-icons";
-import { faRunning } from "@fortawesome/free-solid-svg-icons";
-
 const Section2 = () => {
-  const [isChecked, setIsChecked] = useState(false);
+    const [platforms, setPlatforms] = useState([
+        {
+            id: "youtube",
+            text: "Youtube",
+            checked: false,
+            src: youtube
+        },
+        {
+            id: "twitch",
+            text: "Twitch",
+            checked: false,
+            src: twitch
+        },
+        {
+            id: "afreeca",
+            text: "Afreeca TV",
+            checked: false,
+            src: afreecatv
+        },
+        {
+            id: "vlive",
+            text: "Vlive",
+            checked: false,
+            src: vlive
+        },
+        {
+            id: "naver selective",
+            text: "Naver Selective",
+            checked: false,
+        },
+        {
+            id: "nnaver now",
+            text: "Naver Now",
+            checked: false,
+        },
+        {
+            id: "kakao tv",
+            text: "Kakao TV",
+            checked: false,
+        }
+    ]);
 
-  const onChange = useCallback(
+    const onChange = useCallback(
     (e) => {
-      if (e.target.checked === true) {
-        setIsChecked(e.target.checked);
-        e.target.parentElement.style.boxShadow =
-          "inset -3px -3px 5px rgba(255, 255, 255, 1), inset 3px 3px 5px rgba(0, 0, 0, 0.1)";
-        e.target.parentElement.style.transfrom = "scale(0.95)";
-        e.target.nextElementSibling.style.color = "#fbb30f";
-      } else {
-        setIsChecked(!isChecked);
-        e.target.parentElement.style.boxShadow = "";
-        e.target.parentElement.style.transfrom = "";
-        e.target.nextElementSibling.style.color = "";
-      }
-    },
-    [isChecked]
-  );
+        const { checked, name } = e.target;
+
+        setPlatforms(platforms.map(platform => 
+            platform.id === name ? { ...platform, checked: !platform.checked } : platform
+        ));
+
+        if (checked) {
+            e.target.parentElement.style.boxShadow =
+                "inset -3px -3px 5px rgba(255, 255, 255, 1), inset 3px 3px 5px rgba(0, 0, 0, 0.1)";
+            e.target.parentElement.style.transfrom = "scale(0.95)";
+            e.target.nextElementSibling.style.color = "#fbb30f";
+        } else {
+            e.target.parentElement.style.boxShadow = "";
+            e.target.parentElement.style.transfrom = "";
+            e.target.nextElementSibling.style.color = "";
+        }
+    }, [platforms]);
 
   return (
     <section id="section-2">
-
-<h1>
-          <span>관심 플랫폼</span> 선택
+        <h1>
+        <span>관심 플랫폼</span> 선택
         </h1>
         <p>관심 플랫폼을 선택해주세요!</p>
 
         <ul>
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                name="youtube"
-                id="youtube"
-                checked={isChecked}
-                onChange={onChange}
-              />
-                <img
-                  className="md-icon"
-                  title="YouTube"
-                  alt="YouTube"
-                  src={youtube}
-                />              <span className="checkmark">Youtube</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                name="twitch"
-                id="twitch"
-                checked={isChecked}
-                onChange={onChange}
-              />
-                <img
-                  className="md-icon"
-                  title="Twitch"
-                  alt="Twitch"
-                  src={twitch}
-                />              <span className="checkmark">Twitch</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                name="afreeca"
-                id="afreeca"
-                checked={isChecked}
-                onChange={onChange}
-              />
-                <img
-                  className="md-icon"
-                  title="Afreeca"
-                  alt="Afreeca"
-                  src={afreecatv}
-                />              <span className="checkmark">Afreeca TV</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                name="vlive"
-                id="vlive"
-                checked={isChecked}
-                onChange={onChange}
-              />
-                <img
-                  className="md-icon"
-                  title="Vlive"
-                  alt="Vlive"
-                  src={vlive}
-                />              
-                <span className="checkmark">Vlive</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                name="naver selective"
-                id="naver selective"
-                checked={isChecked}
-                onChange={onChange}
-              />
-                <div
-                  className="md-icon"
-                  title="Naver Selective"
-                  alt="Naver Selective"
-                />               <span className="checkmark">Naver Selective</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                name="naver now"
-                id="naver now"
-                checked={isChecked}
-                onChange={onChange}
-              />
-             
-                                <div className="md-icon" title="Naver Now" alt="Naver Now" />
-
-                <span className="checkmark">Naver Now</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                name="kakao tv"
-                id="kakao tv"
-                checked={isChecked}
-                onChange={onChange}
-              />
-                <div className="md-icon" title="Kakao TV" alt="Kakao TV" />
-              <span className="checkmark">Kakao TV</span>
-            </label>
-          </li>
+            {platforms.map(platform => (
+                <li key={platform.id}>
+                    <label>
+                    <input
+                        type="checkbox"
+                        name={platform.id}
+                        id={platform.id}
+                        checked={platform.checked}
+                        onChange={onChange}
+                    />
+                    {platform.src ? (
+                        <img
+                            className="md-icon"
+                            title={platform.text}
+                            alt={platform.text}
+                            src={platform.src}
+                        />
+                    ) : (
+                        <div className="md-icon" title={platform.text} alt={platform.text} />
+                    )}
+                    
+                    <span className="checkmark">{platform.text}</span>
+                    </label>
+                </li>
+            ))}
         </ul>
-      <ArrowButton />
+        <ArrowButton />
     </section>
-  );
+    );
 };
 
 export default Section2;
