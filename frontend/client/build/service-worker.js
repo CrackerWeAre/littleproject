88e02cf84a831a7424ee51fca5f8e43065c29d12
Primadonna <jwhyun2215@gmail.com>
2020-10-27 23:22:24 +0900

    빌드

--- frontend/client/build/service-worker.js
+++ frontend/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62727f2069a06f88d33534eae73e01a8.js"
+  "/precache-manifest.e66a2d764a694e7380bfd909eaea699c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
